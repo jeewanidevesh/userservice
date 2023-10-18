a3078f7cf9be6629c3d2bdaae7741788ebf1c73a
deveshjeewani <deveshjeewani@gmail.com>
2023-10-18 16:10:30 +0530

    Backend Projects: Authentication and Middlewares dated 16/10/2023 part (done in office)

--- src/main/java/dev/naman/userservicetestfinal/security/SpringSecurity.java
+++ src/main/java/dev/naman/userservicetestfinal/security/SpringSecurity.java
@@ -3,6 +3,7 @@ package dev.naman.userservicetestfinal.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,10 +11,18 @@ public class SpringSecurity {
 
     @Bean
     public SecurityFilterChain filteringCriteria(HttpSecurity http) throws Exception {
+        http.cors().disable();
+        http.csrf().disable();
         http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
 //        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/auth/logout").denyAll());
-        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/auth/logout").authenticated());
+//
+//        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/auth/*").authenticated());
         return  http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
     // Object that handles what all api point should be authenticated

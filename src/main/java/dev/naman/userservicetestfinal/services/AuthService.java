08e7b70ae95bf49197ec9bc431fad0c7d557422c
deveshjeewani <deveshjeewani@gmail.com>
2023-10-29 15:23:36 +0530

    Lecture | Backend Projects: Implementing Search: Paging, Sorting, ElasticSearch dated 23/10/23

--- src/main/java/dev/naman/userservicetestfinal/services/AuthService.java
+++ src/main/java/dev/naman/userservicetestfinal/services/AuthService.java
@@ -6,6 +6,8 @@ import dev.naman.userservicetestfinal.models.SessionStatus;
 import dev.naman.userservicetestfinal.models.User;
 import dev.naman.userservicetestfinal.repositories.SessionRepository;
 import dev.naman.userservicetestfinal.repositories.UserRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.MacAlgorithm;
 import lombok.Setter;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.http.HttpHeaders;
@@ -17,6 +19,10 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.util.MultiValueMapAdapter;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -54,6 +60,40 @@ public class AuthService {
 
         String token = RandomStringUtils.randomAlphanumeric(30);
 
+        MacAlgorithm alg = Jwts.SIG.HS256; //or HS384 or HS256
+        SecretKey key = alg.key().build();
+
+//        String message = "{\n" +
+//                "   \"email\": \"naman@scaler.com\",\n" +
+//                "   \"roles\": [\n" +
+//                "      \"mentor\",\n" +
+//                "      \"ta\"\n" +
+//                "   ],\n" +
+//                "   \"expirationDate\": \"23rdOctober2023\"\n" +
+//                "}";
+        //user_id
+        //email
+        //roles
+//        byte[] content = message.getBytes(StandardCharsets.UTF_8);
+
+        Map<String, Object>  jsonForJwt = new HashMap<>();
+        jsonForJwt.put("email", user.getEmail());
+        jsonForJwt.put("roles", user.getRoles());
+        jsonForJwt.put("createdAt", new Date());
+        jsonForJwt.put("expiryAt", new Date(LocalDate.now().plusDays(3).toEpochDay()));
+
+
+        token = Jwts.builder()
+                .claims(jsonForJwt)
+                .signWith(key, alg)
+                .compact();
+
+// Create the compact JWS:
+//        token = Jwts.builder().content(content, "text/plain").signWith(key, alg).compact();
+
+// Parse the compact JWS:
+//        content = Jwts.parser().verifyWith(key).build().parseSignedContent(jws).getPayload();
+
         Session session = new Session();
         session.setSessionStatus(SessionStatus.ACTIVE);
         session.setToken(token);
@@ -65,6 +105,7 @@ public class AuthService {
         UserDto userDto=UserDto.from(user);
 //        Map<String, String> headers = new HashMap<>();
 //        headers.put(HttpHeaders.SET_COOKIE, token);
+
 
         MultiValueMapAdapter<String, String> headers = new MultiValueMapAdapter<>(new HashMap<>());
         headers.add(HttpHeaders.SET_COOKIE, "auth-token:" + token);
@@ -111,7 +152,28 @@ public class AuthService {
             return null;
         }
 
+        Session session = sessionOptional.get();
+
+        if (!session.getSessionStatus().equals(SessionStatus.ACTIVE)) {
+            return SessionStatus.ENDED;
+        }
+
+
+        Jwts.parser()
+                .build();
+
+
+//        if (!session.)
+
         return SessionStatus.ACTIVE;
     }
 
 }
+
+/// GET /products/1 -> abcd1234abcd
+//        /authentication/validate/abcd1234abcd>u_id=12 -> true
+// if the token is any random strong, we will first need to make
+// a db call to validate the token
+// and then another call to get the details of the user
+// auth-token%3AeyJjdHkiOiJ0ZXh0L3BsYWluIiwiYWxnIjoiSFMyNTYifQ.ewogICAiZW1haWwiOiAibmFtYW5Ac2NhbGVyLmNvbSIsCiAgICJyb2xlcyI6IFsKICAgICAgIm1lbnRvciIsCiAgICAgICJ0YSIKICAgXSwKICAgImV4cGlyYXRpb25EYXRlIjogIjIzcmRPY3RvYmVyMjAyMyIKfQ.r2FVQUCn6DNHir5AlEBT2XQMgO7aN4m3xg9zcuB-zxQ
+// auth-token%3AeyJhbGciOiJIUzI1NiJ9.eyJjcmVhdGVkQXQiOjE2OTgwNzgzNDg0NTQsInJvbGVzIjpbXSwiZXhwaXJ5QXQiOjE5NjU2LCJlbWFpbCI6Im5hbWFuQHNjYWxlci5jb20ifQ._v1af8cc1YA-cEyHlX1BASwveBiASQeteWFM8UzWxfY
